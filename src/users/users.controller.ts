35c205b65875c9b11478c6f2a601018bf8bd2caa
Abram Emad <abram.emad@genwin.net>
2025-05-28 20:24:07 +0300

    parse int pipe

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, Post, Patch, Put, Delete, Param, Query, Body, Ip, Headers } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Put, Delete, Param, Query, Body, Ip, Headers, ParseIntPipe } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-    @Get('/:id/:optional?')
-    public getUsers(@Param('id') id: any, @Query('limit') limit: any) {
+    @Get('/:id?')
+    public getUsers(@Param('id', ParseIntPipe) id: number | undefined, @Query('limit') limit: any) {
         console.log(id);
         console.log(limit);
         return 'You send a get request to users endpoint';
