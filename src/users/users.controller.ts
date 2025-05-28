67d9dd27f4f5210289a95d29cde991d59bc3f366
Abram Emad <abram.emad@genwin.net>
2025-05-28 19:51:28 +0300

    additional req components

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,19 @@
-import { Controller, Get, Post, Patch, Put, Delete, Param, Query, Body } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Put, Delete, Param, Query, Body, Ip, Headers } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
     @Get('/:id/:optional?')
-    public getUsers(@Param() param: any, @Query() query: any) {
-        console.log(param);
-        console.log(query);
+    public getUsers(@Param('id') id: any, @Query('limit') limit: any) {
+        console.log(id);
+        console.log(limit);
         return 'You send a get request to users endpoint';
     }
 
     @Post()
-    public createUsers(@Body() body: any) {
-        console.log(body)
+    public createUsers(@Body() body: any, @Headers() headers: any, @Ip() ip: any) {
+        console.log(body);
+        console.log(headers);
+        console.log(ip);
         return 'You send a post request to users endpoint';
     }
 }
