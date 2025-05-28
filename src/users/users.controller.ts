40308ed8a90313e599f62a90743473cabbd245d7
Abram Emad <abram.emad@genwin.net>
2025-05-28 18:14:27 +0300

    params, query and body

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,17 @@
-import { Controller, Get, Post, Patch, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Put, Delete, Param, Query, Body } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-    @Get()
-    public getUsers() {
+    @Get('/:id/:optional?')
+    public getUsers(@Param() param: any, @Query() query: any) {
+        console.log(param);
+        console.log(query);
         return 'You send a get request to users endpoint';
     }
 
     @Post()
-    public createUsers(){
+    public createUsers(@Body() body: any) {
+        console.log(body)
         return 'You send a post request to users endpoint';
     }
 }
