9a423239bcea5d0299d17dc04a7714109550ba74
Abram Emad <abram.emad@genwin.net>
2025-06-09 16:12:34 +0300

    create user entity

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -23,11 +23,13 @@ export class CreateUserDto {
 
   @IsNotEmpty()
   @IsEmail()
+  @MaxLength(20)
   email: string;
 
   @IsNotEmpty()
   @IsString()
   @MinLength(8)
+  @MaxLength(20)
   @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
     message:
       'Minimum eight characters, at least one letter, one number and one special character',
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -2,18 +2,35 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    firstName: string;
+  @Column({
+    type: 'varchar',
+    length: 10,
+    nullable: false,
+  })
+  firstName: string;
 
-    @Column()
-    lastName: string;
+  @Column({
+    type: 'varchar',
+    length: 10,
+    nullable: true,
+  })
+  lastName: string;
 
-    @Column()
-    email: string;
+  @Column({
+    type: 'varchar',
+    length: 20,
+    nullable: false,
+    unique: true,
+  })
+  email: string;
 
-    @Column()
-    password: string;
+  @Column({
+    type: 'varchar',
+    length: 20,
+    nullable: false,
+  })
+  password: string;
 }
