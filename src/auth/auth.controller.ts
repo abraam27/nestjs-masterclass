20ca602f5d0b661c92e8156bb0fcc996a57e4eb9
Abraam <abraamaj27@gmail.com>
2025-06-22 15:31:19 +0300

    test sign in response

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './providers/auth.service';
 import { ApiTags } from '@nestjs/swagger';
 import { SignInDto } from './dtos/signin.dto';
@@ -9,6 +9,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('sign-in')
+  @HttpCode(HttpStatus.OK)
   public signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
   }
