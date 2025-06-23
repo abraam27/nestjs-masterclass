2ca34f548c61609e30bb6c45eb71518908d1da46
Abraam <abraamaj27@gmail.com>
2025-06-23 15:51:07 +0300

    refresh tokens providers

--- src/auth/providers/refresh-tokens.provider.ts
+++ src/auth/providers/refresh-tokens.provider.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable, NotFoundException, RequestTimeoutException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigType } from '@nestjs/config';
+import jwtConfig from '../config/jwt.config';
+import { RefreshTokenDto } from '../dtos/refresh-token.dto';
+import { GenerateTokensProvider } from './generate-tokens.provider';
+import { UsersService } from 'src/users/providers/users.service';
+import { ActiveUserData } from '../interfaces/active-user-data.interface';
 
 @Injectable()
-export class RefreshTokensProvider {}
+export class RefreshTokensProvider {
+    constructor(
+        private readonly jwtService: JwtService,
+        @Inject(jwtConfig.KEY)
+        private readonly jwtConfigOptions: ConfigType<typeof jwtConfig>,
+        private readonly generateTokensProvider: GenerateTokensProvider,
+        @Inject(forwardRef(() => UsersService))
+        private readonly usersService: UsersService,
+    ) { }
+
+    public async refreshTokens(refreshTokenDto: RefreshTokenDto) {
+        try {
+            // verify the refresh token using jwtService
+            const { sub } = await this.jwtService.verifyAsync<Pick<ActiveUserData, 'sub'>>(refreshTokenDto.refreshToken, {
+                secret: this.jwtConfigOptions.secret,
+                audience: this.jwtConfigOptions.audience,
+                issuer: this.jwtConfigOptions.issuer,
+            });
+            // fetch user from the database using the sub
+            const user = await this.usersService.findOneById(sub);
+            if (!user) {
+                throw new NotFoundException('User not found');
+            }
+            // generate new tokens
+            return await this.generateTokensProvider.generateTokens(user);
+        } catch (error) {
+            throw new RequestTimeoutException(
+                'Unable to process your request at this moment',
+                {
+                    description: 'Could not refresh tokens',
+                },
+            );
+        }
+    }
+}   
