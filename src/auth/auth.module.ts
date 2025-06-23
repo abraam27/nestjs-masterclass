be97f153190b4523bdd9a3c478ee53677cd8f336
Abraam <abraamaj27@gmail.com>
2025-06-23 09:56:34 +0300

    fix auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,6 +7,7 @@ import { BcryptProvider } from './providers/bcrypt.provider';
 import { ConfigModule } from '@nestjs/config';
 import jwtConfig from './config/jwt.config';
 import { JwtModule } from '@nestjs/jwt';
+import { SignInProvider } from './providers/sign-in.provider';
 
 @Module({
   controllers: [AuthController],
@@ -16,6 +17,7 @@ import { JwtModule } from '@nestjs/jwt';
       provide: HashingProvider,
       useClass: BcryptProvider,
     },
+    SignInProvider,
   ],
   exports: [AuthService, HashingProvider],
   imports: [
